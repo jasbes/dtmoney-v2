8ae00c2e0a9fa32c72e62a1b5b0cc77d5851cb77
Jefferson Alves da Silva <jeffthamy@Jeffersons-MacBook-Pro.local>
2023-05-23 14:54:36 +0200

    Add style for summary component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default.ts'
 import { GlobalStyle } from './styles/globals.ts'
 import { Transactions } from './pages/Transactions/index.tsx'
+import { Summary } from './components/Summary/index.tsx'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
+
       <Transactions />
     </ThemeProvider>
   )
