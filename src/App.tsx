6570200a9145e679194f5e23e3debd177babfa1f
Jefferson Alves da Silva <jeffthamy@Jeffersons-MacBook-Pro.local>
2023-05-31 15:27:42 +0200

    Add transactions component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default.ts'
 import { GlobalStyle } from './styles/globals.ts'
 import { Transactions } from './pages/Transactions/index.tsx'
-import { Summary } from './components/Summary/index.tsx'
 
 export function App() {
   return (
