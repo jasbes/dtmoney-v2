d3d7bbf492f70a1c9c9ed17b828e00570ee9fbc0
Jefferson Alves da Silva <jeffthamy@Jeffersons-MacBook-Pro.local>
2023-06-06 15:19:47 +0200

    Add new transaction dialog

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,43 @@
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
+import {
+  Overlay,
+  Content,
+  CloseButton,
+  TransactionType,
+  TransactionTypeButton
+} from './styles'
+import * as Dialog from '@radix-ui/react-dialog'
+
+export function NewTransactionModal() {
+  return (
+    <Dialog.Portal>
+      <Overlay />
+      <Content>
+        <Dialog.Title>Nova transação</Dialog.Title>
+
+        <form>
+          <input type="text" placeholder="Descrição" required />
+          <input type="number" placeholder="Preço" required />
+          <input type="text" placeholder="Categoria" required />
+
+          <TransactionType>
+            <TransactionTypeButton value="income" variant="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+            <TransactionTypeButton value="outcome" variant="outcome">
+              <ArrowCircleDown size={24} />
+              Saida
+            </TransactionTypeButton>
+          </TransactionType>
+
+          <button type="submit">Cadastrar</button>
+        </form>
+
+        <CloseButton>
+          <X size={24} />
+        </CloseButton>
+      </Content>
+    </Dialog.Portal>
+  )
+}
