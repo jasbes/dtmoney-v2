39fe2d4b7b496217f9441485d1d9e727deb53b38
Jefferson <jefferson_uk@hotmail.com>
2023-06-11 18:34:22 +0200

    Add search feature

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -3,6 +3,8 @@ import { SearchFormContainer } from './styles'
 import { useForm } from 'react-hook-form'
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useContext } from 'react'
+import { TransactionsContext } from '../../../../contexts/TransactionsContext.tsx'
 
 const searchFormSchema = z.object({
   query: z.string(),
@@ -11,6 +13,7 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
+  const { fetchTransaction } = useContext(TransactionsContext)
   const {
     register,
     handleSubmit,
@@ -19,7 +22,7 @@ export function SearchForm() {
     resolver: zodResolver(searchFormSchema),
   })
   async function handleSearchTransactions(data: SearchFormInputs) {
-    await new Promise(resolve => setTimeout(resolve, 2000))
+    await fetchTransaction(data.query)
     console.log(data)
   }
 
